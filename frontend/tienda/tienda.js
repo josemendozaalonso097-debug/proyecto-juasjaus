def34aa89cf0e5537ebe2146d60ac6e140f33b11
Jose Mendoza <josemendozaalonso097@gmail.com>
2025-12-16 17:17:59 -0600

    final 1

--- frontend/tienda/tienda.js
+++ frontend/tienda/tienda.js
@@ -49,35 +49,35 @@ window.onload = async function() {
 const productosData = {
     // ... resto del código
     uniformes: [
-        { id: 1, nombre: "Playera Blanca ", marca: "CBTis 258", precio: 350, imagen: "", tallas: true },
-        { id: 2, nombre: "Playera Gris", marca: "CBTis 258", precio: 350, imagen: "", tallas: true },
-        { id: 3, nombre: "Playera Deportiva", marca: "CBTis 258", precio: 280, imagen: "", tallas: true },
-        { id: 4, nombre: "Paquete completo", marca: "CBTis 258", precio: 600, imagen: "", tallas: true },
-        { id: 5, nombre: "Credencial", marca: "Credencial", precio: 100, imagen: "", tallas: false }
+        { id: 1, nombre: "Playera Blanca ", marca: "CBTis 258", precio: 350, imagen: "../imagenesTienda/BackgroundEraser_20251206_214733074.png", tallas: true },
+        { id: 2, nombre: "Playera Gris", marca: "CBTis 258", precio: 350, imagen: "../imagenesTienda/BackgroundEraser_20251216_161735342.png", tallas: true },
+        { id: 3, nombre: "Playera Deportiva", marca: "CBTis 258", precio: 280, imagen: "../imagenesTienda/BackgroundEraser_20251216_161748179.png", tallas: true },
+        { id: 4, nombre: "Paquete completo", marca: "CBTis 258", precio: 600, imagen: "../imagenesTienda/cvtis.png", tallas: true },
+        { id: 5, nombre: "Credencial", marca: "Credencial", precio: 100, imagen: "../imagenesTienda/IMG-20251216-WA0024.jpg", tallas: false }
     ],
 
     Libros: [
-        { id: 6, nombre: "Pensamiento matematico", marca: "Libro", precio: 90, imagen: "", semestre: true },
-        { id: 7, nombre: "Lengua y comunicacion", marca: "Libro", precio: 200, imagen: "", semestre: true },
-        { id: 8, nombre: "Humanidades", marca: "Libro", precio: 100, imagen: "", semestre: true },
-        { id: 9, nombre: "Socio emocional", marca: "Libro", precio: 90, imagen: "", semestre: true },
-        { id: 10, nombre: "Ingles", marca: "Libro", precio: 90, imagen: "", semestre: true },
-        { id: 11, nombre: "Sociales", marca: "Libro", precio: 100, imagen: "", semestre: true },
-        { id: 12, nombre: "Conservacion de la energia", marca: "Libro 2do", precio: 140, imagen: "", tallas: false },
-        { id: 13, nombre: "La materia y sus interacciones", marca: "Libro 1ro", precio: 150, imagen: "", tallas: false },
-        { id: 14, nombre: "Ecosistemas", marca: "Libro 3ro", precio: 150, imagen: "", tallas: false },
-        { id: 15, nombre: "Conciencia historica", marca: "Libros 4to y 5to", precio: 200, imagen: "", semestre: true },
-        { id: 16, nombre: "Reacciones quimicas", marca: "Libro 4to", precio: 200, imagen: "", tallas: false },
-        { id: 17, nombre: "Temas selectos de matematicas", marca: "Libro 4to y 5to", precio: 200, imagen: "", semestre: true },
-        { id: 18, nombre: "La energia en los procesos de la vida diaria", marca: "Libro 5to", precio: 170, imagen: "", tallas: false },
-        { id: 19, nombre: "Temas de filosofia", marca: "Libro 6to", precio: 160, imagen: "", tallas: false }
+        { id: 6, nombre: "Pensamiento matematico", marca: "Libro", precio: 90, imagen: "../imagenesTienda/6475884.png", semestre: true },
+        { id: 7, nombre: "Lengua y comunicacion", marca: "Libro", precio: 200, imagen: "../imagenesTienda/6475884.png", semestre: true },
+        { id: 8, nombre: "Humanidades", marca: "Libro", precio: 100, imagen: "../imagenesTienda/6475884.png", semestre: true },
+        { id: 9, nombre: "Socio emocional", marca: "Libro", precio: 90, imagen: "../imagenesTienda/6475884.png", semestre: true },
+        { id: 10, nombre: "Ingles", marca: "Libro", precio: 90, imagen: "../imagenesTienda/6475884.png", semestre: true },
+        { id: 11, nombre: "Sociales", marca: "Libro", precio: 100, imagen: "../imagenesTienda/6475884.png", semestre: true },
+        { id: 12, nombre: "Conservacion de la energia", marca: "Libro 2do", precio: 140, imagen: "../imagenesTienda/6475884.png", tallas: false },
+        { id: 13, nombre: "La materia y sus interacciones", marca: "Libro 1ro", precio: 150, imagen: "../imagenesTienda/6475884.png", tallas: false },
+        { id: 14, nombre: "Ecosistemas", marca: "Libro 3ro", precio: 150, imagen: "../imagenesTienda/6475884.png", tallas: false },
+        { id: 15, nombre: "Conciencia historica", marca: "Libros 4to y 5to", precio: 200, imagen: "../imagenesTienda/6475884.png", semestre: true },
+        { id: 16, nombre: "Reacciones quimicas", marca: "Libro 4to", precio: 200, imagen: "../imagenesTienda/6475884.png", tallas: false },
+        { id: 17, nombre: "Temas selectos de matematicas", marca: "Libro 4to y 5to", precio: 200, imagen: "../imagenesTienda/6475884.png", semestre: true },
+        { id: 18, nombre: "La energia en los procesos de la vida diaria", marca: "Libro 5to", precio: 170, imagen: "../imagenesTienda/6475884.png", tallas: false },
+        { id: 19, nombre: "Temas de filosofia", marca: "Libro 6to", precio: 160, imagen: "../imagenesTienda/6475884.png", tallas: false }
     ],
     
     tramites: [
-        { id: 20, nombre: "Certificado", marca: "Documento", precio: 150, imagen: "", tallas: false },
-        { id: 21, nombre: "Constancia", marca: "Documento", precio: 50, imagen: "", tallas: false },
-        { id: 22, nombre: "Cardex", marca: "Documento", precio: 30, imagen: "", tallas: false },
-        { id: 23, nombre: "Colegiatura", marca: "Documento", precio: 3000, imagen: "", tallas: false }
+        { id: 20, nombre: "Certificado", marca: "Documento", precio: 150, imagen: "../imagenesTienda/f71decb4816cd27d4460d37b314d2fbf-documento-de-grafico-plano.png", tallas: false },
+        { id: 21, nombre: "Constancia", marca: "Documento", precio: 50, imagen: "../imagenesTienda/f71decb4816cd27d4460d37b314d2fbf-documento-de-grafico-plano.png", tallas: false },
+        { id: 22, nombre: "Cardex", marca: "Documento", precio: 30, imagen: "../imagenesTienda/f71decb4816cd27d4460d37b314d2fbf-documento-de-grafico-plano.png", tallas: false },
+        { id: 23, nombre: "Colegiatura", marca: "Documento", precio: 3000, imagen: "../imagenesTienda/f71decb4816cd27d4460d37b314d2fbf-documento-de-grafico-plano.png", tallas: false }
     ],
 };
 
@@ -1273,7 +1273,7 @@ doc.setFontSize(12);
 doc.text(`Método: Transferencia`, 20, 55);
 doc.text(`Titular: ${nombreTarjeta}`, 20, 65);
 doc.text(`Tarjeta: ${numeroTarjeta}`, 20, 75);
-doc.text(`Fecha: ${fechaFormato}`, 20, 85);
+//doc.text(`Fecha: ${fechaFormato}`, 20, 85);
 
 doc.setFontSize(14);
 doc.setFont(undefined, 'bold');
