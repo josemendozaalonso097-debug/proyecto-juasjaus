37ae76ab5ced733cb6c92297c9c92ec08c33f144
josemendozaalonso097-debug <josemendozaalonso097@gmail.com>
2026-04-22 22:23:07 -0600

    Implement API service with Axios and AsyncStorage

---
+++ mobile/services/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const API_BASE = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8000';
+
+const api = axios.create({
+  baseURL: `${API_BASE}/api`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('access_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
